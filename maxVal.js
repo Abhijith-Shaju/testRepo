c5fcf6301ab2080f2d5af41c374f1e5949cc3d31
Abhijith Shaju <abhijithshaju2004@gmail.com>
2025-11-17 10:56:36 +0530

    this is .

--- maxVal.js
+++ maxVal.js
@@ -2,15 +2,6 @@
 const values = [5, 9, 3, 19, 7];
 // Expected output: 19
 
-
-const max = Math.max(...values);
-// const max = values.reduce( (a, b) => (a > b) ? a : b);
-
-// Find the maximum value in an array
-const values = [5, 9, 3, 19, 7];
-// Expected output: 19
-
-
 const max = Math.max(...values);
 // const max = values.reduce( (a, b) => (a > b) ? a : b);
 
